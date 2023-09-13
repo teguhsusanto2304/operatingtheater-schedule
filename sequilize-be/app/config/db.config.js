1cce3c05ca91913a8db5e7e58af90acea3823978
teguhsusanto2304 <teguh.susanto@hotmail.com>
2023-09-13 18:14:25 +0700

    Update db.config.js

--- sequilize-be/app/config/db.config.js
+++ sequilize-be/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'balimade_user',
+    HOST: 'hostnya',
+    USER: 'usernya',
     PASSWORD: 'done',
-    DB: 'balimade_db',
+    DB: 'dbnya',
     dialect: 'mysql',
     pool: {
         max: 5,
